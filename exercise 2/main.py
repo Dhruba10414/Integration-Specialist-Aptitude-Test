f6902e07668eec4597d8d4f2bcad8167122122c6
Dhruba Mitra <dhruba10414@gmail.com>
2022-05-12 19:56:52 +0600

    Add files via upload

--- exercise 2/main.py
+++ exercise 2/main.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
         str_list.extend(str.split())
 
     # print(str_list)
-
+    total_data_count = 0
     for item in entries:
         content = item['content']
         str = content['title'].lower()
@@ -50,20 +50,29 @@ if __name__ == "__main__":
             'Recurrence Count': recurrence_count
         }
         all_data.append(content_obj)
+        total_data_count += 1
         # all_data.append(article(content['guid'], content['title'], img_list, content['published_at'], content['created_at'], recurrence_count))
         # break
 
     all_data.sort(key = get_my_key, reverse=True)
-
+    # total_data_count = count(all_data)
+    # print(total_data_count)
     cnt = 0
+    check = 0
     field_names = ['Guid', 'Title', 'Related Image Urls', 'Publish Date', 'Creation Date', 'Recurrence Count']
 
     with open('event.csv', 'w') as f_object:
         dictwriter_object = DictWriter(f_object, fieldnames=field_names)
         dictwriter_object.writeheader()
         for item in all_data:
+            if cnt > 3:
+                if item['Recurrence Count'] != check:
+                    f_object.close()
+                    break
             dictwriter_object.writerow(item)
             cnt += 1
-            if cnt == 3:
+            check = item['Recurrence Count']
+
+            if cnt >= total_data_count:
                 f_object.close()
                 break
